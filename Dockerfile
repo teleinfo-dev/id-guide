c7d987b24cd783ea533cd93312a3c78c711b984a
hexiaohuo <hexiaohuo@teleinfo.cn>
2024-04-11 18:03:31 +0800

    feat: 增加/doc路径

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM harbor.idx.space/ops/nginx:1.21.4
 
 COPY ./dist /usr/share/nginx/html/
 
+COPY default.conf /etc/nginx/conf.d/
+
 EXPOSE 80
 
 CMD ["nginx", "-g", "daemon off;"]
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
-  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+  base: '/doc/',
 
   lang: 'zh-CN',
   title: '工业互联网标识解析',
